204087c0e231ec1468ef9d69428f94d939c154b3
Promlert Lovichit <promlert@gmail.com>
2023-07-31 15:54:19 +0700

    Added components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, inject } from '@angular/core';
-import { Choice, Question } from './question';
-import { QuizService } from './quiz.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,50 +6,5 @@ import { QuizService } from './quiz.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  audio = new Audio();
-  quizService: QuizService = inject(QuizService);
-
-  questions: Question[];
-  currentQuestionIndex = 0;
-  isEnd = false;
-  score = 0;
-
-  constructor() {
-    this.questions = this.quizService.getQuizData();
-    this.audio.src = './assets/audio/click.wav';
-    this.newQuiz();
-  }
-
-  onClickChoice(choice: Choice) {
-    console.log('User clicked ' + choice.text);
-
-    this.playSound();
-
-    if (choice.isAnswer) this.score++;
-
-    if (this.currentQuestionIndex < this.questions.length - 1) {
-      this.currentQuestionIndex++;
-      this.questions[this.currentQuestionIndex].choices.sort((a, b) => 0.5 - Math.random());
-    } else {
-      this.isEnd = true;
-    }
-  }
-
-  private playSound() { 
-    this.audio.load();
-    this.audio.addEventListener('canplaythrough', () => {
-      this.audio.play();
-    });
-  }
-
-  onClickNewQuiz() {
-    this.newQuiz();
-  }
-
-  private newQuiz() {
-    this.questions.sort((a, b) => 0.5 - Math.random());
-    this.isEnd = false;
-    this.currentQuestionIndex = 0;
-    this.score = 0;
-  }
+  
 }
